4b13811cd4d26cf08dd26dca65d321d05ed5ba45
Neil Peterson <neilpeterson@outlook.com>
2019-06-03 12:00:13 -0700

    Initial module and tests

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform Modules
+
+Work in progress modules for demonstrating integration with Terratest and Azure Pipelines.
---
+++ modules/hello-world/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  version = "=1.29.0"
+}
+
+resource "azurerm_resource_group" "hello-world" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
+}
+
+resource "azurerm_container_group" "hello-world" {
+  name                = "${var.container-name}"
+  location            = "${azurerm_resource_group.hello-world.location}"
+  resource_group_name = "${azurerm_resource_group.hello-world.name}"
+  ip_address_type     = "public"
+  dns_name_label      = "${var.dns-prefix}"
+  os_type             = "linux"
+
+  container {
+    name   = "hello-world"
+    image  = "${var.container-image}"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port      = 80
+      protocol  = "TCP"
+    }
+
+  }
+}
