5e30f1a8b83d2acfc7b77a4171cc60086e1c878d
Neil Peterson <neilpeterson@outlook.com>
2019-06-05 15:41:09 -0700

    Added random integer

--- modules/hello-world/main.tf
+++ modules/hello-world/main.tf
@@ -7,12 +7,17 @@ resource "azurerm_resource_group" "hello-world" {
   location = "${var.location}"
 }
 
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 resource "azurerm_container_group" "hello-world" {
   name                = "${var.container-name}"
   location            = "${azurerm_resource_group.hello-world.location}"
   resource_group_name = "${azurerm_resource_group.hello-world.name}"
   ip_address_type     = "public"
-  dns_name_label      = "${var.dns-prefix}"
+  dns_name_label      = "${var.dns-prefix}-${random_integer.ri.result}"
   os_type             = "linux"
 
   container {
